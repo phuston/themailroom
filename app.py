535b9084d83872e7e9fe09dd95a2eceed1999f69
Patrick Huston <patrickhuston1@gmail.com>
2017-09-14 01:28:08 -0400

    Super basic functionality all working from start to finish

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, session, redirect, url_for, escape, request, render_template
+from flask_mail import Mail, Message
+import json
+from threading import Thread
+
+from config import MAIL_ADDRESS
+
 
 app = Flask(__name__)
 app.secret_key = 'no one will ever guess this'
+app.config.from_object('config')
+mail = Mail(app)
+
+with open('config/catalog.json') as catalog_file:
+    catalog_list = json.load(catalog_file)
 
 @app.route('/')
 def index():
@@ -21,10 +32,29 @@ def login():
 @app.route('/catalog', methods = ['GET'])
 def catalog():
     if 'email_address' in session:
-        return render_template('catalog.html')
+        return render_template('catalog.html', catalog_list=catalog_list)
     else:
         return redirect(url_for('login'))
 
 @app.route('/order/<item_id>', methods = ['POST'])
 def order(item_id=None):
-    return 'ordered {}'.format(item_id)
+    send_email([session['email_address']], item_id)
+    return redirect(url_for('complete'))
+
+@app.route('/complete', methods = ['GET'])
+def complete():
+    return render_template('complete.html')
+
+
+# Async email support
+def send_async_email(msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(recipients, order_name):
+    subject = "PACKAGE RECEIPT NOTIFICATION"
+    msg = Message(subject, sender=MAIL_ADDRESS, recipients=recipients)
+    msg.body = "YO WHAT UP BITCH! You ordered {}".format(order_name)
+    thr = Thread(target=send_async_email, args=[msg])
+    thr.start()
+
---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# email server
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+MAIL_ADDRESS = os.environ.get('MAIL_ADDRESS')
