bd65139d87363e1d49e61581e40119b8f61e6829
Aidan <aidankmcl@gmail.com>
2017-09-15 16:21:02 -0400

    drink limiting and admin panel for notifications

--- app.py
+++ app.py
@@ -3,11 +3,16 @@ from flask_mail import Mail, Message
 import json
 from threading import Thread
 import time
+from bson.objectid import ObjectId
 import random
+from pymongo import MongoClient
 from datetime import datetime
 
 from config import MAIL_ADDRESS
 
+CLIENT = MongoClient()
+DB = CLIENT.mailroom
+ORDERS = DB.orders
 
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = 'no one will ever guess this'
@@ -39,12 +44,43 @@ def catalog():
     else:
         return redirect(url_for('login'))
 
-@app.route('/order/<item_id>', methods = ['POST'])
-def order(item_id=None):
-    send_email([session['email_address']], item_id)
-    return redirect(url_for('complete'))
 
-@app.route('/complete', methods = ['GET'])
+@app.route('/admin', methods=['GET'])
+def admin():
+    if 'email_address' in session:
+        return render_template('admin.html', orders=list(ORDERS.find({"served": False})))
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/admin/<order_id>', methods=['POST'])
+def serve(order_id):
+    order = ORDERS.find_one({"_id": ObjectId(order_id)})
+    send_email([order['email']], order['drink'])
+    ORDERS.update({"_id": ObjectId(order_id)}, {"served": True})
+    return redirect(url_for('admin'))
+
+
+@app.route('/order/<item_id>', methods=['POST'])
+def order(item_id=None):
+    # Tell them to chill if it's been less than 15 minutes since last order
+    last_drink = (time.time() - session.get('last_drink', 0)) / 60
+    if last_drink < 15:
+        response = {
+            "success": False,
+            "message": "You ordered a drink {} minutes ago, please wait at least 15 minutes between \
+orders #TIPS #StaySafeStayFun #Orientation".format(int(last_drink))
+        }
+    else:
+        session['last_drink'] = time.time()
+        ORDERS.insert({"email": session['email_address'], "drink": item_id, "served": False})
+        response = {
+            "success": True,
+            "message": "Your order has been received. You will get an email from Mailroom Services when it's ready!"
+        }
+    return json.dumps(response)
+
+
+@app.route('/complete', methods=['GET'])
 def complete():
     return render_template('complete.html')
 
@@ -87,4 +123,3 @@ def send_email(recipients, order_name):
                datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     thr = Thread(target=send_async_email, args=[msg])
     thr.start()
-
